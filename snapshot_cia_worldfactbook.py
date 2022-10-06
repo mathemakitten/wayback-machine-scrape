78464b2c333fee51a812052dc65f2d88a3895e43
mathemakitten <helen.ngo14@gmail.com>
2022-10-06 17:19:13 +0100

    world factbook

--- snapshot_cia_worldfactbook.py
+++ snapshot_cia_worldfactbook.py
@@ -1,5 +1,36 @@
+"""
+For each domain in `links_scraped_cia_world_factbook.txt`, for each of <May 2022, June/July 2022, August 2022>,
+get the first day of the month and find the closest snapshot according to the Wayback Machine, then call Wayback Machine
+API again with the timestamp to get the actual content dump. Content dump is saved to list of dicts, which is then
+converted to Pandas and then Parquet on disk.
+"""
+
 import requests
+from bs4 import BeautifulSoup
+import json
 
 api = 'http://archive.org/wayback/available?url=example.com'
 
+with open('/home/helen_huggingface_co/wayback-machine-scrape/links_scraped_cia_world_factbook.txt', 'r') as f:
+    pages = f.read().split('\n')
 
+timestamps = ['20220501', '20220601', '20220801'] # internet archive format is YYYYMMDDhhmmss
+
+txt = ""
+# TODO: multithread this; each worker picks up a (timestamp, page) tuple and pings the API
+for t in timestamps:
+    for i, page in enumerate(pages):
+        x = requests.get(f'http://archive.org/wayback/available?url={page}&timestamp={t}')
+        metadata_dict = json.loads(x.text)
+        closest_timestamp = metadata_dict['timestamp']
+        snapshot_url = metadata_dict['url']
+
+        # scrape actual snapshot content
+        html = requests.get(snapshot_url).content
+        soup = BeautifulSoup(html, "html.parser")
+
+        para = soup.find_all(["p", "h2", "h3"])
+        txt += "".join([str(i) for i in para]) + '\n\n\n'  # figure out how to format text bc headers all over the place
+
+        if i == 20:
+            break
