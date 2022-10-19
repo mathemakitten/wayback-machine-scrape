89fc354cf624da94ceff0e51c12e151ae2c1bd7d
mathemakitten <helen.ngo14@gmail.com>
2022-10-19 11:53:20 -0400

    turn dataset into jsonl

---
+++ scrapes_into_jsonl.py
@@ -0,0 +1,26 @@
+""" Turns files of the format date/pagename.txt into a dataset where the config is the scrape date, with
+columns `page_id` and `text`
+
+OR one massive jsonl where the scrape date is a column and we just append... """
+
+import json
+import os
+
+data_clean = []
+
+dates = os.listdir('factbook')
+for date in dates:
+    pages = os.listdir(os.path.join('factbook', date))
+    for p in pages:
+        fullpath = os.path.join('factbook', date, p, 'text.txt')
+        with open(fullpath, 'r') as f:
+            print(f"Processing: {fullpath}")
+            text = f.read()
+            jsondict = {'snapshot_date': date, 'page_id': p, 'text': text}
+            data_clean.append(jsondict)
+
+with open('factbook/data.jsonl', 'a') as f:
+    for d in data_clean:
+        json.dump(d, f)
+        f.write('\n')
+
