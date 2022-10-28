f32448842e8ef3c479984bd863a2693711817f13
mathemakitten <helen.ngo14@gmail.com>
2022-10-28 14:02:41 -0400

    fix page change script

--- how_often_do_pages_change.py
+++ how_often_do_pages_change.py
@@ -6,13 +6,14 @@ dates = os.listdir('factbook')
 page_ids = [i.split('/')[-1] for i in glob.glob('/home/helen_huggingface_co/wayback-machine-scrape/factbook/20220901/*')]
 
 has_any_page_changed_in_four_months = 0
-total_distinct_pages = 0
+total_distinct_pages = len(set([i.split('/')[-1] for i in glob.glob('/home/helen_huggingface_co/wayback-machine-scrape/factbook/*/*')]))
 
 for page in page_ids:
     files = [os.path.join('factbook', d, page, 'text.txt') for d in dates]
+    print(files)
     comparisons = {}
+    any_file_changed = False
     for itm in range(len(files)):
-        any_file_changed = False
         try:
             res = filecmp.cmp(files[itm], files[itm+1])
             if not res:
@@ -36,15 +37,6 @@ for page in page_ids:
             pass
     if any_file_changed:
         has_any_page_changed_in_four_months += 1
-    total_distinct_pages += 1
-        # try:
-        #     res = filecmp.cmp(files[itm], files[itm+4])
-        #     comparisons[str(files[itm]) + ' vs ' + str(files[itm+4])] = res
-        # except:
-        #     pass
-
-    # print(f"PAGE: {page}")
-    # print(comparisons)
 
     print(f"number of files which have changed anytime over a 4-month window: {has_any_page_changed_in_four_months}")
     print(f"total distinct pages: {total_distinct_pages}")
